a477f0ec77c09a4e8f9a29c3d4953745e0497c39
menachem margolis <margolismendel5728@gmail.com>
2020-12-08 14:36:29 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,33 @@
+require 'pry'
+class Dog
+
+  def initialize(name,breed="Mutt")
+    @breed = breed
+    @name = name
+  end
+ 
+
+  def name=(name)
+    @name=name
+  end
+
+  def name
+    @name
+  end
+  
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed 
+  end
+
+end
+
+
+
+
+
+
